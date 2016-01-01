12740aa0d024f23725df96eea09b7d908a9ebff2
tomkp <tom@tomkp.com>
2016-01-01 16:12:35 +0000

    formatting

--- src/quotes.js
+++ src/quotes.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 export default (symbols) => {
     const symbolStr = symbols.map((s) => `"${s}"`).join(',');
     return axios.get('http://query.yahooapis.com/v1/public/yql', {
-            params: {
-                q: `select * from yahoo.finance.quotes where symbol in (${symbolStr})`,
-                format: 'json',
-                env: 'store://datatables.org/alltableswithkeys'
-            }
-        }).then((response) => {
-            return response.data.query.results.quote;
-});
+        params: {
+            q: `select * from yahoo.finance.quotes where symbol in (${symbolStr})`,
+            format: 'json',
+            env: 'store://datatables.org/alltableswithkeys'
+        }
+    }).then((response) => {
+        return response.data.query.results.quote;
+    });
 }
